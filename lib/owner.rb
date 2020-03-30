868d3bec64db25a6c45789e3fe9e12a767cc021b
Bikram Sodhi <biksodhi@gmail.com>
2020-03-30 16:03:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+
+  attr_accessor
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name, species = "human")
+    @name = name
+    @species= species
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def self.count
+    self.all.count
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
 end
