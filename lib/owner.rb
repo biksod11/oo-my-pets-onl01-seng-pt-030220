7de3c18ef86deda6fdacf67dd1a778fd11c7b1ac
Bikram Sodhi <biksodhi@gmail.com>
2020-03-30 16:14:39 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,6 +20,13 @@ class Owner
     @owner = owner
   end
 
+  def buy_dog(name)
+    new_dog = Dog.new(name, owner = self)
+    @name = name
+    @owner = owner
+  end
+
+
   def cats
     Cat.all.select {|cat| cat.owner == self}
   end
@@ -27,7 +34,7 @@ class Owner
   def dogs
     Dog.all.select {|dog| dog.owner == self}
   end
-  
+
   def name
     @name
   end
